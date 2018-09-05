cbb876e09c9578f4331654feb711ea3bbe864e40
Smriti Srivastava <ssrivastava@tavisca.com>
2018-09-05 14:10:41 +0530

    First Commit

---
+++ FactoryPatternAssignment/FactoryPatternAssignment/CarProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternAssignment
+{
+    class CarProduct : IProduct
+    {
+        public void Book()
+        {
+            Console.WriteLine("Car Product Booked");
+        }
+
+        public string GetTypeOfProduct()
+        {
+            return "Car";
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Car Product Saved");
+        }
+    }
+}
---
+++ FactoryPatternAssignment/FactoryPatternAssignment/ProductFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternAssignment
+{
+    class ProductFactory
+    {
+        public IProduct GetProduct(string productName)
+        {
+            Type type = ProductTypeMap.GetProductType(productName);
+            return (IProduct)Activator.CreateInstance(type);
+        }
+    }
+
+}
